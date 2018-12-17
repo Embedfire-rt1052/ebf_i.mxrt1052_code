f8eed7e5268ff4bab96595ed1746a95497b39c10
pan_gu_lun <1656125469@qq.com>
2018-12-17 09:06:08 +0800

    以RGB灯为模板建立ADC电压采集工程，当前为空

---
+++ 19-ADC—电压采集/1-ADC—单通道（中断读取）/user/main.c
@@ -0,0 +1,105 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   串口中断接收测试
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火  i.MXRT1052开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *CPU_MIMXRT1052DVL6B,PRINTF_FLOAT_ENABLE=1, SCANF_FLOAT_ENABLE=1, PRINTF_ADVANCED_ENABLE=1, SCANF_ADVANCED_ENABLE=1
+  ******************************************************************
+  */
+
+
+#include "fsl_debug_console.h"
+
+#include "board.h"
+#include "pin_mux.h"
+
+#include "clock_config.h"
+#include "./led/bsp_led.h" 
+
+
+
+
+/*******************************************************************
+ * Prototypes
+ *******************************************************************/
+/**
+ * @brief 延时一段时间
+ */
+void delay(uint32_t count);
+
+/*******************************************************************
+ * Code
+ *******************************************************************/
+/**
+ * @note 本函数在不同的优化模式下延时时间不同，
+ *       如flexspi_nor_debug和flexspi_nor_release版本的程序中，
+ *       flexspi_nor_release版本的延时要短得多  
+ */ 
+void delay(uint32_t count)
+{
+  volatile uint32_t i = 0;
+  for (i = 0; i < count; ++i)
+  {
+    __asm("NOP"); /* 调用nop空指令 */
+  }
+}
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+
+  /* 初始化内存保护单元 */
+  BOARD_ConfigMPU();
+  /* 初始化开发板引脚 */
+  BOARD_InitPins();
+  /* 初始化开发板时钟 */
+  BOARD_BootClockRUN();
+  /* 初始化调试串口 */
+  BOARD_InitDebugConsole();
+  /* 打印系统时钟 */
+  
+  PRINTF("\r\n");
+  PRINTF("*****欢迎使用 野火i.MX RT1052 开发板*****\r\n");
+  PRINTF("CPU:             %d Hz\r\n", CLOCK_GetFreq(kCLOCK_CpuClk));
+  PRINTF("AHB:             %d Hz\r\n", CLOCK_GetFreq(kCLOCK_AhbClk));
+  PRINTF("SEMC:            %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SemcClk));
+  PRINTF("SYSPLL:          %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllClk));
+  PRINTF("SYSPLLPFD0:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd0Clk));
+  PRINTF("SYSPLLPFD1:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd1Clk));
+  PRINTF("SYSPLLPFD2:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd2Clk));
+  PRINTF("SYSPLLPFD3:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd3Clk));  
+
+  /* 初始化LED引脚 */
+  LED_GPIO_Config(); 
+
+  while(1)
+  {
+    /* 亮红灯 */
+    RGB_RED_LED_ON;
+    RGB_GREEN_LED_OFF
+    delay(LED_DELAY_COUNT);
+
+    /*亮绿灯*/
+    RGB_RED_LED_OFF
+    RGB_GREEN_LED_ON;
+    delay(LED_DELAY_COUNT);
+  }
+
+}
+
+
+
+/****************************END OF FILE**********************/
+
