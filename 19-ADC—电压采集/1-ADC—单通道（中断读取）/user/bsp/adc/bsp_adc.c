c8c9f3148256710acc26de7c3a85a79358ec7d06
pan_gu_lun <1656125469@qq.com>
2018-12-19 09:41:20 +0800

    添加adc功能

---
+++ 19-ADC—电压采集/1-ADC—单通道（中断读取）/user/bsp/adc/bsp_adc.c
@@ -0,0 +1,9 @@
+
+
+#include "fsl_iomuxc.h"
+#include "fsl_gpio.h"  
+  
+#include "pad_config.h"  
+#include "./adc/bsp_adc.h" 
+
+
---
+++ 19-ADC—电压采集/1-ADC—单通道（中断读取）/user/bsp/adc/bsp_adc.h
@@ -0,0 +1,8 @@
+#ifndef __BSP_ADC_H
+#define __BSP_ADC_H
+
+#include "fsl_common.h"
+
+
+
+#endif /* __BSP_ADC_H */
