18f5d5f29c8d76c405a5b68155eda8ac9ebc82d6
pan_gu_lun <1656125469@qq.com>
2019-01-02 20:01:18 +0800

    基于ADC_ETC PIT定时器作为触发信号工程，新添加工程

---
+++ 19-ADC—电压采集/1-ADC_ETC—PIT定时器作为触发信号使用ADC2/user/bsp/adc/bsp_adc.c
@@ -0,0 +1,212 @@
+
+#include "fsl_debug_console.h"
+#include "fsl_iomuxc.h"
+#include "fsl_gpio.h"  
+#include "fsl_adc.h"
+  
+#include "pad_config.h"  
+#include "./adc/bsp_adc.h" 
+
+
+extern volatile uint32_t g_AdcConversionValue0;
+extern volatile uint32_t g_AdcConversionValue1;
+extern volatile  bool b_Value0_Conversion_complete_flag;
+extern volatile  bool b_Value1_Conversion_complete_flag;
+/**
+* @brief  初始化ADC相关IOMUXC的MUX复用配置
+* @param  无
+* @retval 无
+*/
+static void ADC_IOMUXC_MUX_Config(void)
+{
+  IOMUXC_SetPinMux(CORE_BOARD_ADC_IOMUXC_CH0, 0U);  
+  IOMUXC_SetPinMux(CORE_BOARD_ADC_IOMUXC_CH15, 0U);
+}
+
+/**
+* @brief  初始化ADC相关IOMUXC的MUX复用配置
+* @param  无
+* @retval 无
+*/
+static void ADC_IOMUXC_PAD_Config(void)
+{
+  IOMUXC_SetPinConfig(CORE_BOARD_ADC_IOMUXC_CH0, ADC_PAD_CONFIG_DATA);  
+  IOMUXC_SetPinConfig(CORE_BOARD_ADC_IOMUXC_CH15, ADC_PAD_CONFIG_DATA);   
+}
+
+ /**
+  * @brief  初始化ADC相关的GPIO模式
+  * @param  无
+  * @retval 无
+  */
+static void ADC_IO_Mode_Config(void)
+{
+  /* 定义gpio初始化配置结构体 */
+  gpio_pin_config_t adc_config; 
+  
+  /** ADC，GPIO配置 **/   
+  adc_config.direction = kGPIO_DigitalInput; //输入模式
+  //adc_config.outputLogic =  1;                //默认高电平，在输出模式下配置该选项无效
+  adc_config.interruptMode = kGPIO_NoIntmode; //不使用中断
+  
+  GPIO_PinInit(CORE_BOARD_ADC_GPIO_CH0, CORE_BOARD_ADC_GPIO_PIN_CH0, &adc_config);
+  GPIO_PinInit(CORE_BOARD_ADC_GPIO_CH15, CORE_BOARD_ADC_GPIO_PIN_CH15, &adc_config);
+}
+
+
+ /**
+  * @brief  初始化ADC工作模式
+  * @param  无
+  * @retval 无
+  */
+static void ADC_Mode_Config(void)
+{
+  adc_config_t adcConfigStrcut;                   //定义ADC 模式配置结构体
+  adc_channel_config_t adcChannelConfigStruct;    //ADC 通道配置结构体
+  
+  /*配置工作模式*/
+  ADC_GetDefaultConfig(&adcConfigStrcut);         //获取ADC 默认工作模式
+  adcConfigStrcut.resolution = kADC_Resolution12Bit;
+  
+  ADC_Init(ADCx, &adcConfigStrcut);               //配置ADC工作模式
+  ADC_EnableHardwareTrigger(ADCx, true);          //使能硬件触发模式
+  
+  
+  
+  ADC_Init(ADC1, &adcConfigStrcut);               //配置ADC工作模式
+  ADC_EnableHardwareTrigger(ADC1, true);          //使能硬件触发模式
+  
+
+  
+//  
+//  /*配置转换通道组*/
+//  adcChannelConfigStruct.channelNumber = DEMO_ADC_ETC_CHANNEL0;    //选择与转换通道组关联的转换通道
+//  adcChannelConfigStruct.enableInterruptOnConversionCompleted = false; //禁止转换完成中断
+//  ADC_SetChannelConfig(ADCx, DEMO_ADC_CHANNEL_GROUP0, &adcChannelConfigStruct);
+//  
+//  
+//  adcChannelConfigStruct.channelNumber = DEMO_ADC_ETC_CHANNEL1;
+//  ADC_SetChannelConfig(ADCx, DEMO_ADC_CHANNEL_GROUP1, &adcChannelConfigStruct);
+
+
+  /*配置转换通道组*/
+  adcChannelConfigStruct.channelNumber = 16;    //选择与转换通道组关联的转换通道
+  adcChannelConfigStruct.enableInterruptOnConversionCompleted = false; //禁止转换完成中断
+  ADC_SetChannelConfig(ADCx, DEMO_ADC_CHANNEL_GROUP0, &adcChannelConfigStruct);
+  
+  
+  adcChannelConfigStruct.channelNumber = 16;
+  ADC_SetChannelConfig(ADCx, DEMO_ADC_CHANNEL_GROUP1, &adcChannelConfigStruct);
+  
+  
+  
+  adcChannelConfigStruct.channelNumber = 16;    //选择与转换通道组关联的转换通道
+  adcChannelConfigStruct.enableInterruptOnConversionCompleted = false; //禁止转换完成中断
+  ADC_SetChannelConfig(ADC1, DEMO_ADC_CHANNEL_GROUP0, &adcChannelConfigStruct);
+  
+  
+  adcChannelConfigStruct.channelNumber = 16;
+  ADC_SetChannelConfig(ADC1, DEMO_ADC_CHANNEL_GROUP1, &adcChannelConfigStruct);
+  
+  
+  
+  
+
+  /*进行硬件校准*/
+  while(kStatus_Success != ADC_DoAutoCalibration(ADCx))
+  {
+    PRINTF("校准失败 \r\n");
+  }
+  PRINTF("校准完成\r\n");                                                                                               
+  
+
+}
+
+/*配置为允许外部触发*/
+void ADC_ETC_Config(void)
+{
+  adc_etc_config_t adcEtcConfig;//配置外部触发控制器
+  adc_etc_trigger_config_t adcEtcTriggerConfig; //配置外部触发控制器的触发属性。主要包括优先级、触发方式、触发通道数量
+  adc_etc_trigger_chain_config_t adcEtcTriggerChainConfig; // 配置外部触发控制器具体的触发通道
+  
+  
+  /*配置外部触发控制器*/
+  ADC_ETC_GetDefaultConfig(&adcEtcConfig);
+  
+  adcEtcConfig.enableTSCBypass = false;//设置该位允许使用ADC2
+  
+  adcEtcConfig.XBARtriggerMask = 1<<ADC_ETC_CHANNEL_GROUPx; /* 并且选择了触发通道为触发通道0*/
+  ADC_ETC_Init(DEMO_ADC_ETC_BASE, &adcEtcConfig);
+  
+  
+  /*  配置外部触发通道。*/
+  adcEtcTriggerConfig.enableSyncMode = true;                      //使能同步模式，同步：ADC1和ADC2被相同的触发源控制
+  adcEtcTriggerConfig.enableSWTriggerMode = false;                 //使能软件触发模式
+  adcEtcTriggerConfig.triggerChainLength = DEMO_ADC_ETC_CHAIN_LENGTH; /* Chain length is 2.设置有多少个ADC通道参与转换 */
+  adcEtcTriggerConfig.triggerPriority = 0U;                         //外部触发优先级
+  adcEtcTriggerConfig.sampleIntervalDelay = 0U;                     //设置采样时间间隔                
+  adcEtcTriggerConfig.initialDelay = 0U;                            //设置触发器初始延时
+  
+  ADC_ETC_SetTriggerConfig(DEMO_ADC_ETC_BASE, ADC_ETC_CHANNEL_GROUPx, &adcEtcTriggerConfig);//设置外部XBAR触发器配置
+  
+  
+  
+  /*配置具体的转换通道*/
+  
+  /*****************************************************************************************************************************/
+  adcEtcTriggerChainConfig.enableB2BMode = true;
+  adcEtcTriggerChainConfig.ADCHCRegisterSelect = 1U<< DEMO_ADC_CHANNEL_GROUP0;         //选择要触发的ADC_HC1寄存器
+  adcEtcTriggerChainConfig.ADCChannelSelect = 0;                   // ADC采样通道
+  adcEtcTriggerChainConfig.InterruptEnable = kADC_ETC_Done0InterruptEnable;                // 使能该通道的转换完成中断 
+  ADC_ETC_SetTriggerChainConfig(DEMO_ADC_ETC_BASE, ADC_ETC_CHANNEL_GROUPx, 0U, &adcEtcTriggerChainConfig); //
+ /*****************************************************************************************************************************/
+
+
+  /****************************************************************************************************************************/                             
+  adcEtcTriggerChainConfig.ADCHCRegisterSelect = 1U<< DEMO_ADC_CHANNEL_GROUP1;         //选择要触发的ADC_HC1寄存器. 
+  adcEtcTriggerChainConfig.ADCChannelSelect = 15;                   //ADC采样通道 
+  adcEtcTriggerChainConfig.InterruptEnable = kADC_ETC_Done1InterruptEnable;            //使能当前通道转换完成中断.
+  ADC_ETC_SetTriggerChainConfig(DEMO_ADC_ETC_BASE, ADC_ETC_CHANNEL_GROUPx, 1U,&adcEtcTriggerChainConfig);  //
+  /********************************************************************************************************************************/
+
+  /* Enable the NVIC. */
+  EnableIRQ(ADC_ETC_IRQ1_IRQn);
+  EnableIRQ(ADC_ETC_IRQ0_IRQn);
+
+}
+
+
+
+/**
+  * @brief  初始化控制ADC的IO
+  * @param  无
+  * @retval 无
+  */
+void ADC_Config(void)
+{
+  
+  ADC_IOMUXC_MUX_Config();//设置引脚功能
+  ADC_IOMUXC_PAD_Config();//设置引脚配置参数，驱动强度、转换速率等
+  ADC_IO_Mode_Config();   //设置引脚模式，输入/输出，是否开启中断等
+  ADC_Mode_Config();      //初始化ADC工作模式，并且进行硬件校准。
+  
+  ADC_ETC_Config(); //配置外部触发
+
+}
+
+/*中断服务函数*/
+void EXAMPLE_ADC_ETC_DONE0_Handler(void)
+{
+  ADC_ETC_ClearInterruptStatusFlags(DEMO_ADC_ETC_BASE, (adc_etc_external_trigger_source_t)ADC_ETC_CHANNEL_GROUPx, kADC_ETC_Done0StatusFlagMask);
+  g_AdcConversionValue0 = ADC_ETC_GetADCConversionValue(DEMO_ADC_ETC_BASE, ADC_ETC_CHANNEL_GROUPx, 0U); /* Get trigger0 chain0 result. */
+  b_Value0_Conversion_complete_flag = true;
+}
+
+void EXAMPLE_ADC_ETC_DONE1_Handler(void)
+{
+  ADC_ETC_ClearInterruptStatusFlags(DEMO_ADC_ETC_BASE, (adc_etc_external_trigger_source_t)ADC_ETC_CHANNEL_GROUPx, kADC_ETC_Done1StatusFlagMask);
+  g_AdcConversionValue1 = ADC_ETC_GetADCConversionValue(DEMO_ADC_ETC_BASE, ADC_ETC_CHANNEL_GROUPx, 1U); /* Get trigger0 chain1 result. */ 
+  b_Value1_Conversion_complete_flag = true;
+}
+
+
---
+++ 19-ADC—电压采集/1-ADC_ETC—PIT定时器作为触发信号使用ADC2/user/bsp/adc/bsp_adc.h
@@ -0,0 +1,103 @@
+#ifndef __BSP_ADC_H
+#define __BSP_ADC_H
+
+#include "fsl_common.h"
+#include "fsl_adc.h"
+#include "fsl_adc_etc.h"
+
+/*********************************************************
+ * ADC GPIO端口、引脚号及IOMUXC复用宏定义
+ 
+ 使用i.MX RT1052-Pro底板：
+ *  ADC1 CH0-----GPIO_AD_B1_11,J13-----(CN5, 29)
+ *  CH15 CH15----GPIO_AD_B1_10,l13-----(CN5, 31)
+ *引脚功能         引脚标号        引脚在开发板上对应位置
+ * CH0 引脚对应开发板CN5排针的第29脚(CN5, 29)
+ * CH15引脚对应开发板CN5排针的第31脚(CN5, 31)
+
+使用i.MX RT1052-Mini底板
+ *  ADC1 CH0-----GPIO_AD_B1_11,J13-----(CN4, 29)
+ *  CH15 CH15----GPIO_AD_B1_10,l13-----(CN4, 31)
+ *引脚功能         引脚标号        引脚在开发板上对应位置
+ * CH0 引脚对应开发板CN4排针的第29脚(CN4, 29)
+ * CH15引脚对应开发板CN4排针的第31脚(CN4, 31) 
+
+ *********************************************************/
+ //GPIO_AD_B1_11
+#define CORE_BOARD_ADC_GPIO_CH0             GPIO1
+#define CORE_BOARD_ADC_GPIO_PIN_CH0         (27U)
+#define CORE_BOARD_ADC_IOMUXC_CH0          IOMUXC_GPIO_AD_B1_11_GPIO1_IO27
+
+ //GPIO_AD_B1_10
+#define CORE_BOARD_ADC_GPIO_CH15             GPIO1
+#define CORE_BOARD_ADC_GPIO_PIN_CH15         (26U)
+#define CORE_BOARD_ADC_IOMUXC_CH15           IOMUXC_GPIO_AD_B1_10_GPIO1_IO26
+ 
+
+
+// 可以是 ADC1/2，
+#define    ADCx                          ADC2
+
+
+
+
+/**
+*设置ADC_ETC转换组
+*每个ADC_ETC有7个转换通道组(0到7)，每个ADC_ETC转换通道组最多可添加7个ADC转换通道。
+*当一个ADC_ETC转换通道组收到触发信号时开始转换该通道组指定的ADC转换通道.
+*/
+#define ADC_ETC_CHANNEL_GROUPx 3
+
+
+/**
+*定义ADC转换通道组
+*每个ADC有8个这样的转换通道（0到7），每个转换通道只能指定一个输入通道（一个ADC有16个输入通道）
+*/
+#define DEMO_ADC_CHANNEL_GROUP0 4U
+#define DEMO_ADC_CHANNEL_GROUP1 5U
+
+
+/**
+*定义ADC输入通道，每个ADC有16个输入通道（0到15）对应到芯片的不同引脚。
+*/
+#define DEMO_ADC_ETC_CHANNEL0 15U
+#define DEMO_ADC_ETC_CHANNEL1 0U
+
+#define DEMO_ADC_ETC_BASE ADC_ETC
+#define DEMO_ADC_ETC_CHAIN_LENGTH 1U /* Chain length is 2. */
+
+
+
+#define EXAMPLE_ADC_ETC_DONE0_Handler ADC_ETC_IRQ0_IRQHandler
+#define EXAMPLE_ADC_ETC_DONE1_Handler ADC_ETC_IRQ1_IRQHandler
+/*******************************************************************************
+ * ADC引脚PAD配置
+ ******************************************************************************/
+#define ADC_PAD_CONFIG_DATA            (SRE_0_SLOW_SLEW_RATE| \
+                                        DSE_6_R0_6| \
+                                        SPEED_2_MEDIUM_100MHz| \
+                                        ODE_0_OPEN_DRAIN_DISABLED| \
+                                        PKE_0_PULL_KEEPER_DISABLED| \
+                                        PUE_0_KEEPER_SELECTED| \
+                                        PUS_0_100K_OHM_PULL_DOWN| \
+                                        HYS_0_HYSTERESIS_DISABLED)   
+    /* 配置说明 : */
+    /* 转换速率: 转换速率慢
+      驱动强度: R0/6 
+      带宽配置 : medium(100MHz)
+      开漏配置: 关闭 
+      拉/保持器配置: 关闭
+      拉/保持器选择: 保持器（上面已关闭，配置无效）
+      上拉/下拉选择: 100K欧姆下拉（上面已关闭，配置无效）
+      滞回器配置: 关闭 */ 
+
+static void ADC_IOMUXC_MUX_Config(void);
+static void ADC_IOMUXC_PAD_Config(void);
+static void ADC_IO_Mode_Config(void);
+static void ADC_Mode_Config(void);
+
+
+void ADC_Config(void);
+
+#endif /* __BSP_ADC_H */
+
