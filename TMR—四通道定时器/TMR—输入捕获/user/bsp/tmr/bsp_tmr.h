97ede05b8d24899ccdbbc3768090a04e505587fd
pan_gu_lun <1656125469@qq.com>
2019-01-11 11:25:49 +0800

    实现输入捕获

--- TMR—四通道定时器/TMR—输入捕获/user/bsp/tmr/bsp_tmr.h
+++ TMR—四通道定时器/TMR—输入捕获/user/bsp/tmr/bsp_tmr.h
@@ -1,15 +1,70 @@
 #ifndef __BSP_TMR_H
 #define __BSP_TMR_H
 
+#include "fsl_iomuxc.h"
 #include "fsl_common.h"
 #include "fsl_qtmr.h"
 
+/*********************************************************
+ * TMR 输入端口、引脚号及IOMUXC复用宏定义
+ *********************************************************/
+/*
+*GPIO_AD_B1_01, I2C1_SCL, CON 1X40TFT DISPLAY,  AT24C02, (CN4, 21),HDR254F-2X10摄像头
+*/
+#define TMR_TIMER0_INPUT_GPIO                GPIO1
+#define TMR_TIMER0_INPUT_GPIO_PIN            (16U)
+#define TMR_TIMER0_INPUT_IOMUXC              IOMUXC_GPIO_AD_B1_00_QTIMER3_TIMER0
 
-/* 定义 TMR 定时器的通道和工作模式instance/channel used for board */
+
+/*
+*GPIO_AD_B1_01, I2C1_SDA, CON 1X40TFT DISPLAY,  AT24C02, (CN4, 23),HDR254F-2X10摄像头
+*/
+#define TMR_TIMER1_INPUT_GPIO                GPIO1
+#define TMR_TIMER1_INPUT_GPIO_PIN            (17U)
+#define TMR_TIMER1_INPUT_IOMUXC              IOMUXC_GPIO_AD_B1_01_QTIMER3_TIMER1
+
+
+
+/* 输入捕获引脚的PAD配置 */
+#define TMR_INPUT_PAD_CONFIG_DATA       (SRE_0_SLOW_SLEW_RATE| \
+                                        DSE_6_R0_6| \
+                                        SPEED_2_MEDIUM_100MHz| \
+                                        ODE_0_OPEN_DRAIN_DISABLED| \
+                                        PKE_1_PULL_KEEPER_ENABLED| \
+                                        PUE_0_KEEPER_SELECTED| \
+                                        PUS_2_100K_OHM_PULL_UP| \
+                                        HYS_0_HYSTERESIS_DISABLED)   
+    /* 配置说明 : */
+    /* 转换速率: 转换速率慢
+      驱动强度: R0/6 
+      带宽配置 : medium(100MHz)
+      开漏配置: 关闭 
+      拉/保持器配置: 开启
+      拉/保持器选择: 保持器
+      上拉/下拉选择: 100K上拉
+      滞回器配置: 关闭 */     
+
+// 定时器输入捕获用户自定义变量结构体声明
+typedef struct
+{   
+	uint8_t   Capture_FinishFlag;   // 捕获结束标志位
+	uint8_t   Capture_StartFlag;    // 捕获开始标志位
+	uint32_t  Capture_CcrValue_1;     // 捕获寄存器的值
+  uint32_t   Capture_CcrValue_2;     // 捕获寄存器的值
+	uint16_t  Capture_Period;       // 定时器溢出次数 
+}GPT_ICUserValueTypeDef;
+
+
+
+
+/* 定义 TMR 定时器的通道和工作模式 */
 #define BOARD_QTMR_BASEADDR TMR3
-#define BOARD_FIRST_QTMR_CHANNEL kQTMR_Channel_0
-#define BOARD_SECOND_QTMR_CHANNEL kQTMR_Channel_1
-#define QTMR_ClockCounterOutput kQTMR_ClockCounter0Output
+#define BOARD_QTMR_INPUT_CAPTURE_CHANNEL kQTMR_Channel_0
+#define BOARD_QTMR_PWM_CHANNEL kQTMR_Channel_1
+#define QTMR_CounterInputPin kQTMR_Counter0InputPin
+
+
+
 
 /* 中断号和中断服务函数定义 */
 #define QTMR_IRQ_ID TMR3_IRQn
@@ -19,12 +74,10 @@
 #define QTMR_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_IpgClk)
 
 
-/*定义定时时间（单位：ms）*
-*注意:TMR定时器的计数寄存器是16位，使用kCLOCK_IpgClk时钟，选择最大时钟分频（128分频）
-*最多实现63ms的定时。
-*/
-#define TMR_TIMIER 50
+/*定义自动重装在值*/
+#define RELODE_COUNTER 0xffff
 
+void TMR_GPIO_Config(void);
 void TMR_Init(void);
 
 
--- TMR—四通道定时器/TMR—输入捕获/user/main.c
+++ TMR—四通道定时器/TMR—输入捕获/user/main.c
@@ -23,7 +23,8 @@
 #include "./led/bsp_led.h"   
 #include "./tmr/bsp_tmr.h"
 
-extern volatile bool qtmrIsrFlag;
+
+extern volatile GPT_ICUserValueTypeDef GPT_ICUserValueStructure;
 /*******************************************************************
  * Prototypes
  *******************************************************************/
@@ -56,6 +57,7 @@ void delay(uint32_t count)
   */
 int main(void)
 {
+    uint64_t timer = 0;
     /* 初始化内存保护单元 */
     BOARD_ConfigMPU();
     /* 初始化开发板引脚 */
@@ -65,6 +67,7 @@ int main(void)
     /* 初始化调试串口 */
     BOARD_InitDebugConsole();
     /* 打印系统时钟 */
+  
     PRINTF("\r\n");
     PRINTF("*****欢迎使用 野火i.MX RT1052 开发板*****\r\n");
     PRINTF("CPU:             %d Hz\r\n", CLOCK_GetFreq(kCLOCK_CpuClk));
@@ -76,21 +79,31 @@ int main(void)
     PRINTF("SYSPLLPFD2:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd2Clk));
     PRINTF("SYSPLLPFD3:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd3Clk));  
   
-    PRINTF("TMR定时器定时 50ms\r\n");
+    PRINTF("TMR定时器定时输入捕获r\n");
   
     /* 初始化LED引脚 */
-    LED_GPIO_Config();  
+    LED_GPIO_Config() ;
     TMR_Init();
       
-    while(1)
-    {
-       while (!(qtmrIsrFlag))
-       {
-         
-       }
-       PRINTF("\r\n Timer interrupt has occured !");
-       qtmrIsrFlag = false;
-    }     
+   while(1)
+   {
+      /*判断是否捕获完成*/
+      if(GPT_ICUserValueStructure.Capture_FinishFlag)
+      {
+
+       /*得到计数值，timer 为64位数据，32位很可能会溢出*/
+       timer = GPT_ICUserValueStructure.Capture_Period * 0xffff; 
+       timer += GPT_ICUserValueStructure.Capture_CcrValue_2;         
+       timer -= GPT_ICUserValueStructure.Capture_CcrValue_1;
+       
+       /*将计数值转化为时间，单位（ms）*/
+       timer = (timer*1000) / ((QTMR_SOURCE_CLOCK)/128);
+       
+       PRINTF("the result is: %lld ms \r\n",timer);
+       GPT_ICUserValueStructure.Capture_FinishFlag = 0;
+      }   
+
+   }     
 
 }
 /****************************END OF FILE**********************/
