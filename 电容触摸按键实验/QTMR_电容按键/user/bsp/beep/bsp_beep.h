5a9178361f579ee5d056e8f38ea0e038bfe5981e
pan_gu_lun <1656125469@qq.com>
2019-05-05 09:47:47 +0800

    添加Systick相关文件

--- 电容触摸按键实验/QTMR_电容按键/user/bsp/beep/bsp_beep.h
+++ 电容触摸按键实验/QTMR_电容按键/user/bsp/beep/bsp_beep.h
@@ -2,13 +2,36 @@
 #define	__BEEP_H_
 
 #include "fsl_common.h"
+#include "fsl_iomuxc.h"
+#include "pad_config.h" 
 
 /*********************************************************
  * BEEP GPIO端口、引脚号及IOMUXC复用宏定义
  *********************************************************/
-#define BOARD_BEEP_GPIO 				    GPIO1
+#define BOARD_BEEP_GPIO 		    GPIO1
 #define BOARD_BEEP_GPIO_PIN 		    (3U)
-#define BOARD_BEEP_IOMUXC			      IOMUXC_GPIO_AD_B0_03_GPIO1_IO03
+#define BOARD_BEEP_IOMUXC	            IOMUXC_GPIO_AD_B0_03_GPIO1_IO03
+
+
+/* 设置BEEP引脚使用的PAD配置 */
+#define BEEP_PAD_CONFIG_DATA            (SRE_0_SLOW_SLEW_RATE| \
+                                        DSE_6_R0_6| \
+                                        SPEED_2_MEDIUM_100MHz| \
+                                        ODE_0_OPEN_DRAIN_DISABLED| \
+                                        PKE_0_PULL_KEEPER_DISABLED| \
+                                        PUE_0_KEEPER_SELECTED| \
+                                        PUS_0_100K_OHM_PULL_DOWN| \
+                                        HYS_0_HYSTERESIS_DISABLED)   
+    /* 配置说明 : */
+    /* 转换速率: 转换速率慢
+      驱动强度: R0/6 
+      带宽配置 : medium(100MHz)
+      开漏配置: 关闭 
+      拉/保持器配置: 关闭
+      拉/保持器选择: 保持器（上面已关闭，配置无效）
+      上拉/下拉选择: 100K欧姆下拉（上面已关闭，配置无效）
+      滞回器配置: 关闭 */  
+
 
 /******************** BEEP 函数宏定义 **************************/
 #define BEEP_ON   GPIO_PinWrite(BOARD_BEEP_GPIO, BOARD_BEEP_GPIO_PIN, 1U);
