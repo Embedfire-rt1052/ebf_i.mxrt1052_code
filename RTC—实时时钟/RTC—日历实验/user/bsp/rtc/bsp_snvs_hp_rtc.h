4780644cf5f6a2cf6e7960a358cc026bf6541340
wangjinl <892461474@qq.com>
2019-07-02 11:48:41 +0800

    增加串口输入报警时间说明 修改两个实验的必读说明 在本地将两个实验工程合并在RTC-实时时钟目录下 修改部分注释

---
+++ RTC—实时时钟/RTC—日历实验/user/bsp/rtc/bsp_snvs_hp_rtc.h
@@ -0,0 +1,26 @@
+#ifndef _BSP_SNVS_HP_RTC_H_
+#define _BSP_SNVS_HP_RTC_H_
+
+#include "fsl_common.h"
+
+void RTC_Config(void);
+void RTC_AlarmTest(void);
+void RTC_TimeAndDate_Show(void);
+
+//中断相关，IRQ中断号及IRQHandler中断服务函数
+#define kCLOCK_SnvsHp0 kCLOCK_SnvsHp
+#define EXAMPLE_SNVS_IRQn SNVS_HP_WRAPPER_IRQn
+#define EXAMPLE_SNVS_IRQHandler SNVS_HP_WRAPPER_IRQHandler
+
+// 时间宏定义
+#define HOURS                8U            // 0~23
+#define MINUTES              0             // 0~59
+#define SECONDS              0             // 0~59
+
+// 日期宏定义
+#define YEAR                 2018U         // 1970~2099
+#define MONTH                04U           // 1~12
+#define DATE                 21U           // 1~31
+
+#endif /* _BSP_SNVS_HP_RTC_H_ */
+
